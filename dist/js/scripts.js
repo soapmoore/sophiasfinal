6fdfc53b729284d7000e69acc8b9957a75ba8ff6
Sophia Moore <sms1390@nyu.edu>
2021-07-06 00:36:27 -0400

    hi

---
+++ dist/js/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener("click", clickHandlers);
+
+function home() {
+  window.location.href = "index.html";
+}
+
+function about() {
+  window.location.href = "about.html";
+}
+
+function contact() {
+  window.location.href = "contact.html";
+}
+
+function pets() {
+  window.location.href = "pets.html";
+}
+
+function pictures() {
+  window.location.href = "pictures.html";
+}
